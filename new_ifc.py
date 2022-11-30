4bdce46fb0d7e1581a3318210952a291e316e4e6
cheshiren <cheshire@ifiction.ru>
2022-11-30 21:17:26 +0500

    Changes to be committed: 	modified:   new_ifc.py

--- new_ifc.py
+++ new_ifc.py
@@ -19,10 +19,16 @@ pO = 0., 0., 0.
 dX = 1., 0., 0.
 dY = 0., 1., 0.
 dZ = 0., 0., 1.
+adX =-1., 0., 0.
+adY = 0., -1., 0.
+adZ = 0., 0., -1.
 
 globalAxisX = ifc.createIfcDirection(dX)
 globalAxisY = ifc.createIfcDirection(dY)
 globalAxisZ = ifc.createIfcDirection(dZ)
+globalAxisXa = ifc.createIfcDirection(adX)
+globalAxisYa = ifc.createIfcDirection(adY)
+globalAxisZa = ifc.createIfcDirection(adZ)
 originPoint = ifc.createIfcCartesianPoint(pO)
 
 worldCoordinateSystem = ifc.createIfcAxis2Placement3D(originPoint, globalAxisZ, globalAxisX)
@@ -33,7 +39,7 @@ PRWidth = 6000.0
 PRSpans = [12000.0,21000.0,6000.0,9000.0,6000.0,9000.0,6000.0,9000.0]
 PRLength = sum(PRSpans)
 PRLevels = {
-    'Ground Level': 0.,
+    'Ground Level': 00.,
     'Tier 1': 6400.,
     'Tier 2': 10000.
 }
@@ -415,38 +421,150 @@ for _srp in srps:
 
 # Создание колонн
 
+rectProfiles = []
+
+def createRectProfile(
+    _XDim=600.,
+    _YDim=600.,
+):
+    _profile = ifc.createIfcRectangleProfileDef()
+    _profile.ProfileType = 'AREA'
+    _profile.ProfileName = f'{_XDim}x{_YDim}'
+    _profile.XDim, _profile.YDim = _XDim, _YDim
+    rectProfiles.append(_profile)
+    return _profile
+
+
+# Cardinal points:
+#   Y
+# 7 8 9
+# 4 5 6 X
+# 1 2 3
+
+cardinalPoints = []
+cardinalPointsCoords = []
+
+def findCardinalPointCoords(xd=100., yd=100., index=5):
+    if index == 1:
+        x = -xd/2
+        y = -yd/2
+    elif index == 2:
+        x = -xd/2
+        y = 0.
+    elif index == 3:
+        x = xd/2
+        y = -yd/2
+    elif index == 4:
+        x = 0.
+        y = -yd/2
+    elif index == 5:
+        x = 0.
+        y = 0.
+    elif index == 6:
+        x = 0.
+        y = yd/2
+    elif index == 7:
+        x = -xd/2
+        y = yd/2
+    elif index == 8:
+        x = 0.
+        y = yd/2.
+    elif index == 9:
+        x = xd/2
+        y = yd/2
+    point = [x, y, 0.]
+    return point
+
+
+extrudedAreaSolids = []
+
+def createExtrudedAreaSolid(
+    _XDim=600.,
+    _YDim=600.,
+    _Depth=6000.,
+    _CardinalIndex=5,
+    _StartCut=0.,
+    _EndCut=0.,
+#    _Axis=globalAxisZ
+):
+    _profile=None
+    for p in rectProfiles:
+        if _XDim == p.XDim and _YDim == p.YDim:
+            _profile = p
+    if _profile == None:
+        _profile = createRectProfile(_XDim, _YDim)
+    _cardinalPoint=None
+    _cardinalPointCoords=findCardinalPointCoords(_XDim, _YDim, _CardinalIndex)
+    _cardinalPointCoords[2] += _StartCut
+    for cp in cardinalPoints:
+        if list(cp.Coordinates) == _cardinalPointCoords:
+            _cardinalPoint = cp
+    if _cardinalPoint == None:
+        _cardinalPoint = ifc.createIfcCartesianPoint(_cardinalPointCoords)
+        cardinalPoints.append(_cardinalPoint)
+        cardinalPointsCoords.append(_cardinalPointCoords)
+    _eas = ifc.createIfcExtrudedAreaSolid()
+    _eas.SweptArea = _profile
+    _eas.Position = ifc.createIfcAxis2Placement3d(_cardinalPoint)
+    _eas.ExtrudedDirection = globalAxisZ
+    _eas.Depth = _Depth - _StartCut - _EndCut
+    extrudedAreaSolids.append(_eas)
+    return _eas
+    
+
+productDefShapes = []
+
+def ColumnPDSCreation(
+    _XDim=600.,
+    _YDim=600.,
+    _Depth=6000.,
+#    _Axis=globalAxisZ
+):
+    _columnEAS=None
+    for eas in extrudedAreaSolids:
+        if (_XDim == eas.SweptArea.XDim and
+            _YDim == eas.SweptArea.YDim and 
+            _Depth == eas.Depth
+            ):
+            _columnEAS = eas
+    if _columnEAS == None:
+        _columnEAS = createExtrudedAreaSolid(_XDim, _YDim, _Depth, 5)
+    
+    _columnPDS=None
+    for pds in productDefShapes:
+        if pds.Representations[0].Items[0] == _columnEAS:
+            _columnPDS = pds
+    if _columnPDS == None:
+        _columnSP = ifc.createIfcShapeRepresentation()
+        _columnSP.ContextOfItems = bodyContext
+        _columnSP.RepresentationIdentifier = 'Body'
+        _columnSP.RepresentationType = 'SweptSolid'
+        _columnSP.Items = [_columnEAS]
+        _columnPDS = ifc.createIfcProductDefinitionShape()
+        _columnPDS.Name = f"{_XDim}x{_YDim}x{_Depth}"
+        _columnPDS.Description = f"Column of {_XDim}x{_YDim} rectangular section and {_Depth} height"
+        _columnPDS.Representations = [_columnSP]
+        
+    productDefShapes.append(_columnPDS)
+    return _columnPDS
+
 
 def ColumnCreation(
     _XDim=600.,
     _YDim=600.,
     _Depth=6000.,
+    _Axis=globalAxisZ,
     _Name='Precast Column',
     _Tag='PCC',
     _RelatingStructure=site,
-    _Side=None
-):
-    _columnProfile = ifc.createIfcRectangleProfileDef()
-    _columnProfile.ProfileType = 'AREA'
-    _columnProfile.ProfileName = f'{_XDim}x{_YDim}'
-    _columnProfile.XDim, _columnProfile.YDim = _XDim, _YDim
-
-    _columnEAS = ifc.createIfcExtrudedAreaSolid()
-    _columnEAS.SweptArea = _columnProfile
-    _columnEAS.ExtrudedDirection = globalAxisZ
-    _columnEAS.Depth = _Depth
-
-    _columnSP = ifc.createIfcShapeRepresentation()
-    _columnSP.ContextOfItems = bodyContext
-    _columnSP.RepresentationIdentifier = 'Body'
-    _columnSP.RepresentationType = 'SweptSolid'
-    _columnSP.Items = [_columnEAS]
-
-    _columnPDS = ifc.createIfcProductDefinitionShape()
-    _columnPDS.Representations = [_columnSP]
-
+    _Side=None,
+):    
+    _columnPlacement = ifc.createIfcLocalPlacement()
+    _columnPlacement.PlacementRelTo = _RelatingStructure.ObjectPlacement
+    _columnPlacement.RelativePlacement = ifc.createIfcAxis2Placement3d(_Side,_Axis)
     
-    _columnPlacement = create_ifclocalplacement(point=_Side,relativeTo=_RelatingStructure.ObjectPlacement)
-
+    _columnPDS = ColumnPDSCreation(_XDim,_YDim,_Depth)
+    
     _column = ifc.createIfcColumn(ios.guid.new(), ownerHistory)
     _column.Name = _Name
     _column.Tag = _Tag
@@ -456,24 +574,190 @@ def ColumnCreation(
 
     return _column
 
+
+
+
+def BeamPDSCreation(
+    _XDim=500.,
+    _YDim=700.,
+    _Depth=6000.,
+    _CardinalIndex=0,
+    _StartCut=0.,
+    _EndCut=0.,
+):
+    _beamEAS=None
+    for eas in extrudedAreaSolids:
+        if ( _XDim == eas.SweptArea.XDim and
+            _YDim == eas.SweptArea.YDim and 
+             _Depth == eas.Depth
+            ):
+            _beamEAS = eas
+    if _beamEAS == None:
+        _beamEAS = createExtrudedAreaSolid(
+            _XDim,
+            _YDim,
+            _Depth,
+            _CardinalIndex,
+            _StartCut,
+            _EndCut,
+        )
+    
+    _beamPDS=None
+    for pds in productDefShapes:
+        if pds.Representations[0].Items[0] == _beamEAS:
+            _beamPDS = pds
+    if _beamPDS == None:
+        _beamSP = ifc.createIfcShapeRepresentation()
+        _beamSP.ContextOfItems = bodyContext
+        _beamSP.RepresentationIdentifier = 'Body'
+        _beamSP.RepresentationType = 'SweptSolid'
+        _beamSP.Items = [_beamEAS]
+        _beamPDS = ifc.createIfcProductDefinitionShape()
+        _beamPDS.Name = f"{_XDim}x{_YDim}x{_Depth}"
+        _beamPDS.Description = f"Beam of {_XDim}x{_YDim} rectangular section and {_Depth} length"
+#        _beamPDS.Description = f"{cardinalPoints}"
+        _beamPDS.Representations = [_beamSP]
+        
+    productDefShapes.append(_beamPDS)
+    return _beamPDS
+
+def BeamCreation(
+    _XDim=500.,
+    _YDim=700.,
+    _Depth=6000.,
+    _Axis=globalAxisZ,
+    _Name='Precast Beam',
+    _Tag='PCB',
+    _RelatingStructure=site,
+    _Side=None,
+    _CardinalIndex=0,
+    _StartCut=0.,
+    _EndCut=0.,
+):    
+    _beamPlacement = ifc.createIfcLocalPlacement()
+    _beamPlacement.PlacementRelTo = _RelatingStructure.ObjectPlacement
+    _beamPlacement.RelativePlacement = ifc.createIfcAxis2Placement3d(_Side,_Axis)
+    
+    _beamPDS = BeamPDSCreation(
+        _XDim,
+        _YDim,
+        _Depth,
+        _CardinalIndex,
+        _StartCut,
+        _EndCut,
+    )
+    
+    _beam = ifc.createIfcBeam(ios.guid.new(), ownerHistory)
+    _beam.Name = _Name
+    _beam.Tag = _Tag
+    _beam.PredefinedType = 'BEAM'
+    _beam.ObjectPlacement = _beamPlacement
+    _beam.Representation = _beamPDS
+    
+    return _beam
+
+
+
+
+
+
 LeftPoint = ifc.createIfcCartesianPoint([0., PRWidth/2, PRLevels['Ground Level']])
 RightPoint = ifc.createIfcCartesianPoint([0., -PRWidth/2, PRLevels['Ground Level']])
+Tier1Point = ifc.createIfcCartesianPoint([0., -PRWidth/2, PRLevels['Tier 1']])
+Tier2Point = ifc.createIfcCartesianPoint([0., -PRWidth/2, PRLevels['Tier 2']])
 
 for _frame in frames:
-    _Tag = 'PCC' + str(frames.index(_frame) + 1)
+    _columnSize = 600.,600.
+    _TagC = 'PCC' + str(frames.index(_frame) + 1)
+    _TagB = 'PCB' + str(frames.index(_frame) + 1)
     _Depth = PRLevels['Tier 2']-PRLevels['Ground Level'],
     # _Depth, = _Depth
     _Depth = _Depth[0]
-    _leftColumn = ColumnCreation(_RelatingStructure=_frame, _Tag=_Tag, _Depth=_Depth, _Side=LeftPoint)
-    _rightColumn = ColumnCreation(_RelatingStructure=_frame, _Tag=_Tag, _Depth=_Depth, _Side=RightPoint)
-    _columnsContainer = ifc.createIfcRelContainedInSpatialStructure()
-    _columnsContainer.RelatingStructure = _frame
-    _columnsContainer.RelatedElements = [_leftColumn, _rightColumn]
+#    _columnPDS = ColumnPDSCreation(600.,600.,_Depth,globalAxisY)
+    _leftColumn = ColumnCreation(
+        _XDim=_columnSize[0],
+        _YDim=_columnSize[1],
+        _Depth=_Depth,
+        _Axis=globalAxisZ,
+        _RelatingStructure=_frame,
+        _Tag=_TagC,
+        _Side=LeftPoint
+    )
+    _rightColumn = ColumnCreation(
+        _XDim=_columnSize[0],
+        _YDim=_columnSize[1],
+        _Depth=_Depth,
+        _Axis=globalAxisZ,
+        _RelatingStructure=_frame,
+        _Tag=_TagC,
+        _Side=RightPoint
+    )
+    _lowerBeam = BeamCreation(
+        _XDim=500.,
+        _YDim=700.,
+        _Depth=PRWidth,
+        _Axis=globalAxisY,
+        _RelatingStructure=_frame,
+        _Tag=_TagB,
+        _Side=Tier1Point,
+        _CardinalIndex=8,
+        _StartCut=_columnSize[0]/2,
+        _EndCut=_columnSize[0]/2,
+    )
+    _upperBeam = BeamCreation(
+        _XDim=500.,
+        _YDim=700.,
+        _Depth=PRWidth,
+        _Axis=globalAxisY,
+        _RelatingStructure=_frame,
+        _Tag=_TagB,
+        _Side=Tier2Point,
+        _CardinalIndex=8,
+        _StartCut=_columnSize[0]/2,
+        _EndCut=_columnSize[0]/2,
+    )
+    _elementsContainer = ifc.createIfcRelContainedInSpatialStructure()
+    _elementsContainer.RelatingStructure = _frame
+    _elementsContainer.RelatedElements = [_leftColumn, _rightColumn, _lowerBeam, _upperBeam]
+
+
+
+
+#beamP = ifc.createIfcRectangleProfileDef()
+#beamP.ProfileType = 'AREA'
+##beamP.ProfileName = '600*600'
+#beamP.XDim, beamP.YDim = 500., 700.
+#beamEAS = ifc.createIfcExtrudedAreaSolid()
+#beamEAS.SweptArea = beamP
+#beamEAS.ExtrudedDirection = globalAxisZ
+#beamEAS.Depth = 6000.
+#beamPos = ifc.createIfcCartesianPoint([0.,350.,0.])
+#beamEAS.Position = ifc.createIfcAxis2Placement3d(beamPos)
+#beamSP = ifc.createIfcShapeRepresentation()
+#beamSP.ContextOfItems = bodyContext
+#beamSP.RepresentationIdentifier = 'Body'
+#beamSP.RepresentationType = 'SweptSolid'
+#beamSP.Items = [beamEAS]
+#beamPDS = ifc.createIfcProductDefinitionShape()
+#beamPDS.Representations = [beamSP]
+
+#beamPlacement = ifc.createIfcLocalPlacement()
+#beamPlacement.PlacementRelTo = frames[1].ObjectPlacement
+#beamPlacement.RelativePlacement = ifc.createIfcAxis2Placement3d(originPoint,globalAxisY)
+
+#beam = ifc.createIfcBeam(ios.guid.new(), ownerHistory)
+#beam.ObjectPlacement = beamPlacement
+#beam.Representation = beamPDS
+
+#beamContainer = ifc.createIfcRelContainedInSpatialStructure()
+#beamContainer.RelatingStructure = frames[1]
+#beamContainer.RelatedElements = [beam]
 
 # print(project)
 
 # path = 'H:\\PY\\new_model.ifc'
-path = '/Users/vdobranov/Yandex.Disk.localized/Python/Mac/ifcopenshell/new_model.ifc'
+path = 'D:\\DELETEME\\new_model.ifc'
+#path = '/Users/vdobranov/Yandex.Disk.localized/Python/Mac/ifcopenshell/new_model.ifc'
 ifc.write(path)
 
 def load_ifc_automatically(f):
